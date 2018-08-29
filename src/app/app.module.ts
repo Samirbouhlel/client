44ab88b19d6e5dfdf830ba598ac5691da052c34b
Samirbouhlel <samirbouhlel1994@gmail.com>
2018-08-29 14:10:58 +0100

    crud produit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {ReactiveFormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ProduitComponent } from './produit/produit.component';
 import {ProduitMockService} from './produit/produit.mock.service';
-import {Produit} from './shared/produit';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { ContentComponent } from './content/content.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import {AppRoutingModule} from './app.routing.module';
+import{ProduitService} from './produit/produit.service';
+import {HttpClientModule} from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent,
@@ -20,9 +21,9 @@ import {AppRoutingModule} from './app.routing.module';
     DashboardComponent
   ],
   imports: [
-    BrowserModule,AppRoutingModule
+    BrowserModule,AppRoutingModule,ReactiveFormsModule,HttpClientModule
   ],
-  providers: [ProduitMockService],
+  providers: [ProduitMockService,ProduitService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -2,12 +2,16 @@ import { NgModule} from '@angular/core';
 import { RouterModule, Routes} from '@angular/router';
 import { ProduitComponent } from './produit/produit.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-
+import {ProduitResolver} from './produit/produit.resolver';
 
 
 export const appRoutes: Routes=[
 {path:'',redirectTo:'/dashboard',pathMatch:'full'},
-{path:'produit',component:ProduitComponent},
+{path:'produit',
+component:ProduitComponent,
+resolve: {
+    produits: ProduitResolver
+}},
 {path:'dashboard',component:DashboardComponent}
 
 ]
@@ -15,7 +19,8 @@ export const appRoutes: Routes=[
 imports:[
     RouterModule.forRoot(appRoutes,{enableTracing:true})
 ],
-exports:[RouterModule ]
+exports:[RouterModule ],
+providers: [ProduitResolver]
 })
 export class AppRoutingModule{
 
